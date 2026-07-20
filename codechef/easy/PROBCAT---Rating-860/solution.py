4cfd4a494ee1364181d64b770426be0389d102c0
TanishMehta <tanishmehta66@gmail.com>
2026-07-20 15:20:54 +0530

    ✅ PROBCAT - Rating 860 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-20

---
+++ codechef/easy/PROBCAT---Rating-860/solution.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    a=int(input())
+    if 1<=a<100:
+        print("Easy")
+    elif 100<=a<200:
+        print("Medium")
+    elif 200<=a<=300:
+        print("Hard")
