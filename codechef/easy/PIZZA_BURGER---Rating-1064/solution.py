6e1d2ce2091a059529e2f9cc933fe1d46d211518
TanishMehta <tanishmehta66@gmail.com>
2026-07-21 17:57:22 +0530

    ✅ PIZZA_BURGER - Rating 1064 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-21

--- codechef/easy/PIZZA_BURGER---Rating-1064/solution.py
+++ codechef/easy/PIZZA_BURGER---Rating-1064/solution.py
@@ -3,9 +3,9 @@ for _ in range(t):
     a,b,c=map(int,input().split())
     if a>=b and a>=c:
         print("PIZZA")
-    elif a>b:
+    elif a>=b:
         print("PIZZA")
-    elif a>c:
+    elif a>=c:
         print("BURGER")
     else:
         print("NOTHING ")
