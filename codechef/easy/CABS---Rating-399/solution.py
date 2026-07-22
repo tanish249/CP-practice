3018ab6453e03625d41b37944d375a5456ad2829
TanishMehta <tanishmehta66@gmail.com>
2026-07-22 11:04:58 +0530

    ✅ CABS - Rating 399 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-22

--- codechef/easy/CABS---Rating-399/solution.py
+++ codechef/easy/CABS---Rating-399/solution.py
@@ -3,7 +3,7 @@ for _ in range(t):
     a,b=map(int,input().split())
     if a>b:
         print("SECOND")
-    elif a<b:
-        print('FIRST')
-    elif a==b:
+    elif b>a:
+        print("FIRST")
+    else:
         print("ANY")
