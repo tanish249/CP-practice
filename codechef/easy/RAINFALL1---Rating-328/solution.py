73a6c5fc261ff2c5f1cb36206e21a16376374e16
TanishMehta <tanishmehta66@gmail.com>
2026-07-22 12:09:33 +0530

    ✅ RAINFALL1 - Rating 328 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-22

--- codechef/easy/RAINFALL1---Rating-328/solution.py
+++ codechef/easy/RAINFALL1---Rating-328/solution.py
@@ -3,7 +3,7 @@ for _ in range(t):
     a=int(input())
     if 3>a:
         print("LIGHT")
-    elif a>=3 and a<7:
+    elif a>=3 and 7>a:
         print("MODERATE")
     else:
         print("HEAVY")
